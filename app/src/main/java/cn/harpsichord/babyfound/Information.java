328b900d335250c68acadb6f9d708bc07da36b21
Harpsichord <feelingsxxx@gmail.com>
2022-02-09 15:26:12 +0800

    list view

---
+++ app/src/main/java/cn/harpsichord/babyfound/Information.java
@@ -0,0 +1,10 @@
+package cn.harpsichord.babyfound;
+
+public class Information {
+
+    public String imageURL;
+    public String informationText;
+    public double longitude;
+    public double latitude;
+
+}
--- app/src/main/java/cn/harpsichord/babyfound/MainActivity.java
+++ app/src/main/java/cn/harpsichord/babyfound/MainActivity.java
@@ -1,14 +1,68 @@
 package cn.harpsichord.babyfound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Information> informationArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i=0; i<5; i++) {
+            Information information = new Information();
+            information.imageURL = "test_image_url";
+            information.informationText = "Text" + i;
+            informationArrayList.add(information);
+        }
+
+        final ListView listview = (ListView) findViewById(R.id.publish_information_list);
+        listview.setAdapter(
+                new InformationAdapter(this, informationArrayList)
+        );
+
+
     }
+}
+
+class InformationAdapter extends ArrayAdapter<Information> {
+
+    Context context;
+    List<Information> data;
+
+    public InformationAdapter(Context context, List<Information> data) {
+        super(context, -1, data);
+        this.context = context;
+        this.data = data;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.information_item, parent, false);
+        ImageView imageView = rowView.findViewById(R.id.baby_image);
+        TextView textView = rowView.findViewById(R.id.baby_text);
+
+        imageView.setImageResource(R.drawable.test_baby);
+        textView.setText(data.get(position).informationText);
+        return rowView;
+    }
+
 }
