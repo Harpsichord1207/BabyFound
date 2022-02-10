3ed4d601e7603d8b98c0e8335896bb6f9eea7cda
Harpsichord <feelingsxxx@gmail.com>
2022-02-10 18:36:29 +0800

    save

--- app/src/main/java/cn/harpsichord/babyfound/PublishActivity.java
+++ app/src/main/java/cn/harpsichord/babyfound/PublishActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,17 +20,31 @@ import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+
+import okhttp3.FormBody;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PublishActivity extends AppCompatActivity {
 
     public ImageView imageView;
     public TextView locTextView;
+    public EditText editText;
 
     public LocationClient mLocationClient = null;
     public MapView mapView = null;
     public BaiduMap baiduMap = null;
+
+    public InputStream image = null;
+
     private final MyLocationListener myListener = new MyLocationListener();
 
     @Override
@@ -37,6 +52,7 @@ public class PublishActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
 
+        editText = findViewById(R.id.input_text);
         mapView = findViewById(R.id.bmapView);
         baiduMap = mapView.getMap();
         baiduMap.setMyLocationEnabled(true);
@@ -55,7 +71,12 @@ public class PublishActivity extends AppCompatActivity {
 
         Button submitBtn = findViewById(R.id.submit_information);
         submitBtn.setOnClickListener(v -> {
-            Toast.makeText(PublishActivity.this, "Wait Backend!", Toast.LENGTH_SHORT).show();
+            try {
+                uploadInformation();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         });
 
         mLocationClient = new LocationClient(this);
@@ -77,15 +98,17 @@ public class PublishActivity extends AppCompatActivity {
         if (!(requestCode == 100 && resultCode == RESULT_OK)) {
             return;
         }
-
         try {
             InputStream inputStream = this.getContentResolver().openInputStream(data.getData());
             Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
             bitmap = Bitmap.createScaledBitmap(bitmap,  400 ,400, true);
             imageView.setImageBitmap(bitmap);
-        } catch (FileNotFoundException e) {
+            image = inputStream;
+        } catch (FileNotFoundException e){
             e.printStackTrace();
         }
+
+
     }
 
     @Override
@@ -102,8 +125,73 @@ public class PublishActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+        mLocationClient.start();
+        baiduMap.setMyLocationEnabled(false);
         mapView.onDestroy();
+        mapView = null;
+        super.onDestroy();
+    }
+
+    public void uploadInformation() throws IOException {
+        MyLocationData locationData = baiduMap.getLocationData();
+        double longitude = locationData.longitude;
+        double latitude = locationData.latitude;
+        String text = editText.getText().toString();
+
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println(latitude);
+        System.out.println(longitude);
+        System.out.println(text);
+        System.out.println(image);
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .addFormDataPart("details", text)
+                .addFormDataPart("longitude", String.valueOf(longitude))
+                .addFormDataPart("latitude", String.valueOf(latitude))
+                .addFormDataPart("FILES", "xx.jpg",  RequestBody.create(toByteArray(image)))
+                .build();
+
+
+        Request request = new Request.Builder()
+                .url("http://52.81.88.46:8000/upload/")
+                .post(requestBody)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        new Thread(() -> {
+            try {
+                Response response = client.newCall(request).execute();
+                // TODO: 到详情页
+
+                System.out.println("============================");
+                System.out.println(latitude);
+                System.out.println(longitude);
+                System.out.println(text);
+                System.out.println(image);
+                System.out.println(response.body().string());
+                System.out.println("============================");
+
+                customToast("发布成功！");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    private void customToast(String content) {
+        // TODO: 默认参数Toast.LENGTH_SHORT
+        runOnUiThread(() -> Toast.makeText(PublishActivity.this, content, Toast.LENGTH_SHORT).show());
+    }
+
+    public static byte[] toByteArray(InputStream input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[4096];
+        int n = 0;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+        }
+        return output.toByteArray();
     }
 
 }
