be39a71e95b6f236a861442246b850459c38abaa
Harpsichord <feelingsxxx@gmail.com>
2022-02-10 17:32:00 +0800

    list data from api

--- app/src/main/java/cn/harpsichord/babyfound/MainActivity.java
+++ app/src/main/java/cn/harpsichord/babyfound/MainActivity.java
@@ -19,10 +19,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,28 +40,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i=0; i<25; i++) {
-            Information information = new Information();
-            information.imageURL = "test_image_url";
-            information.informationText = "Text" + i;
-            informationArrayList.add(information);
-        }
-
         ListView listview = findViewById(R.id.publish_information_list);
         listview.setOnItemClickListener((parent, view, position, id) -> {
             Information selectedItem = (Information) parent.getItemAtPosition(position);
             // TODO: Redirect to detail Page
             Toast.makeText(MainActivity.this, selectedItem.informationText, Toast.LENGTH_SHORT).show();
         });
-        listview.setAdapter(new InformationAdapter(this, informationArrayList));
-
+        InformationAdapter informationAdapter = new InformationAdapter(this, informationArrayList);
+        listview.setAdapter(informationAdapter);
+        informationAdapter.notifyDataSetChanged();
         Button publishBtn = findViewById(R.id.publish_information_btn);
         publishBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, PublishActivity.class);
             startActivity(intent);
         });
 
+        getAllInformation();
+
+        Button refreshBtn = findViewById(R.id.refresh_information);
+        refreshBtn.setOnClickListener(v -> {
+            getAllInformation();
+        });
+
     }
+
+    public void getAllInformation() {
+        Request request = new Request.Builder()
+                .url("http://52.81.88.46:8000/get_all/")
+                .get()
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        new Thread(() -> {
+            try {
+                Response response = client.newCall(request).execute();
+                String body = response.body().string();
+                JsonObject jsonBody = JsonParser.parseString(body).getAsJsonObject();
+                for (JsonElement ele: jsonBody.getAsJsonArray("data")) {
+                    String imageURL = ele.getAsJsonObject().get("img_url").getAsString();
+                    String text = ele.getAsJsonObject().get("img_details").getAsString();
+                    double longitude = ele.getAsJsonObject().get("img_longitude").getAsDouble();
+                    double latitude = ele.getAsJsonObject().get("img_latitude").getAsDouble();
+
+                    Information information = new Information();
+                    information.informationText = text;
+                    information.imageURL = imageURL;
+                    information.longitude = longitude;
+                    information.latitude = latitude;
+
+                    informationArrayList.add(information);
+                }
+                // Toast.makeText(MainActivity.this, "刷新成功，获取到" + informationArrayList.size() + "条信息", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
 }
 
 class InformationAdapter extends ArrayAdapter<Information> {
@@ -77,8 +118,9 @@ class InformationAdapter extends ArrayAdapter<Information> {
 
         // imageView.setImageResource(R.drawable.test_baby);
         // 使用Picasso通过URL设置图片
-        Picasso.with(context).load("https://www-cdn.cigdata.cn/gkbf/online_test.jpg").error(R.drawable.test_baby).into(imageView);
-        textView.setText(data.get(position).informationText);
+        Information information = data.get(position);
+        Picasso.with(context).load(information.imageURL).error(R.drawable.test_baby).into(imageView);
+        textView.setText(information.informationText);
         return rowView;
     }
 
